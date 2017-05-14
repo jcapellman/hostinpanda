ed56dc0382a0bdd7eccf0ce8fe9861a9f1602add
Jarred Capellman <jcapellman@hotmail.com>
2017-05-13 21:18:38 -0500

    Added: HostCheckLog Table

---
+++ src/hostinpanda.serverlibrary/DAL/Tables/HostCheckLog.cs
@@ -0,0 +1,9 @@
+﻿namespace hostinpanda.serverlibrary.DAL.Tables
+{
+    public class HostCheckLog : BaseTable
+    {
+        public int HostID { get; set; }
+
+        public bool IsUp { get; set; }
+    }
+}
