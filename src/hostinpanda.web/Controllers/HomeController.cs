1c75b60430c76f3a5c1028ae0a7017612b8d57b0
Jarred Capellman <jcapellman@hotmail.com>
2017-05-15 20:58:11 -0500

    Change: Removed About Action

--- src/hostinpanda.web/Controllers/HomeController.cs
+++ src/hostinpanda.web/Controllers/HomeController.cs
@@ -15,13 +15,6 @@ namespace hostinpanda.web.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Register()
         {
             return View();
