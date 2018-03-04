e6d6cc8b2a6b75d7fa18e4c15ed6d16b0f4ecc66
Jarred Capellman <jcapellman@hotmail.com>
2018-03-04 10:03:37 -0600

    Added: EF Migration Class to handle migration generation and running

---
+++ src/hostinpanda.web/DAL/DALdbContextMigration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace hostinpanda.web.DAL
+{
+    public class DALdbContextMigration : IDesignTimeDbContextFactory<DALdbContext>
+    {
+        public DALdbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DALdbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=XXX;user id=XXX;password=XXX");
+
+            return new DALdbContext(optionsBuilder.Options);
+        }
+    }
+}
