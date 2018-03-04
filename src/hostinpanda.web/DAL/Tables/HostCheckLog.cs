fcb819e7716ce3153a7129ba64059dcffabf6833
Jarred Capellman <jcapellman@hotmail.com>
2018-03-04 09:55:49 -0600

    Fix: Foreign Key for Hosts in the HostCheckLog

--- src/hostinpanda.web/DAL/Tables/HostCheckLog.cs
+++ src/hostinpanda.web/DAL/Tables/HostCheckLog.cs
@@ -1,7 +1,14 @@
-﻿namespace hostinpanda.web.DAL.Tables
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hostinpanda.web.DAL.Tables
 {
     public class HostCheckLog : BaseTable
     {
+        [Required]
+        [ForeignKey("HostID")]
+        public Hosts Host { get; set; }
+
         public int HostID { get; set; }
 
         public bool IsUp { get; set; }
