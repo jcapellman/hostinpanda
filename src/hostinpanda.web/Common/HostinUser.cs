fc863f7dc800f9e490e0382c36afaf86102187f5
Jarred Capellman <jcapellman@hotmail.com>
2018-03-11 17:42:03 -0500

    Added: HostinUser class that extends the ClaimsPrincipal class

---
+++ src/hostinpanda.web/Common/HostinUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace hostinpanda.web.Common
+{
+    public class HostinUser : ClaimsPrincipal
+    {
+        public HostinUser(ClaimsPrincipal claimsPrincipal) : base(claimsPrincipal)
+        {
+        }
+
+        public int? ID
+        {
+            get
+            {
+
+                var userClaim = Claims.FirstOrDefault();
+
+                if (userClaim == null)
+                {
+                    return null;
+                }
+
+                return (int?)Convert.ToInt32(userClaim.Value);
+            }
+        }
+    }
+}
