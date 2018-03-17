b83e28bab7b5c8d8dfe25f5a03a87cb41a4e0466
Jarred Capellman <jcapellman@hotmail.com>
2018-03-16 19:54:02 -0500

    Added: ConfigObject class to handle parsing the config.json file

---
+++ src/hostinpanda.daemon/ConfigObject.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+using System.IO;
+
+namespace hostinpanda.daemon
+{
+    public class ConfigObject
+    {
+        public string DatabaseConnectionString { get; set; }
+
+        public string SMTPHostName { get; set; }
+
+        public string SMTPUsername { get; set; }
+
+        public string SMTPPassword { get; set; }
+
+        public int SMTPPort { get; set; }
+
+        public static ConfigObject Load(string configFile)
+        {
+            if (!File.Exists(configFile))
+            {
+                return default(ConfigObject);
+            }
+
+            return JsonConvert.DeserializeObject<ConfigObject>(File.ReadAllText(configFile));
+        }
+    }
+}
